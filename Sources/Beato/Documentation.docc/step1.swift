e54969aee05412ab092c55fc0ae09719aab6b043
Sevki <s@sevki.io>
2022-06-15 19:19:26 +0100

    karplus-strong

---
+++ Sources/Beato/Documentation.docc/step1.swift
@@ -0,0 +1,24 @@
+//
+//  Karplus-Strong.swift
+//
+//
+//  Created by Sevki Hasirci on 15/06/2022.
+//
+
+import Beato
+import Foundation
+
+struct KarplusStrong: Synthesizer {
+    let sampleRate: Double
+    let frequency: Double
+    let duration: Double
+}
+
+// Go version of KS https://github.com/timiskhakov/music/blob/master/karplusstrong/basic.go
+extension KarplusStrong {
+    func synthesize() -> [Float] {
+        let noise = (0 ..< Int(sampleRate / frequency)).map {
+            Float(arc4random_uniform(UInt32.max)) / Float(UInt32.max)
+        }
+    }
+}
