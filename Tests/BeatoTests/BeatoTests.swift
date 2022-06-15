70c5f9638ca3f771a8477a75507f93d419f2716b
Sevki <s@sevki.io>
2022-06-15 16:05:31 +0100

    Update BeatoTests.swift

--- Tests/BeatoTests/BeatoTests.swift
+++ Tests/BeatoTests/BeatoTests.swift
@@ -3,6 +3,6 @@ import XCTest
 
 final class BeatoTests: XCTestCase {
     func testExample() throws {
-        XCTAssertEqual(Beato.MIDI.Notes.A⋔, Pitch(hertz: 440.0))
+        XCTAssertEqual(Note(number: 69)⋔, Pitch(hertz: 440.0))
     }
 }
